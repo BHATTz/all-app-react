b00214e9037c78056b0b7250af1e948b92bff196
BHATTz <sarthakbhatt0@gmail.com>
2024-08-29 17:51:50 +0530

    up

--- src/component/home-learning/learnlogic.jsx
+++ src/component/home-learning/learnlogic.jsx
@@ -3,11 +3,12 @@ import { Link } from "react-router-dom";
 
 export default function learnlogic() {
   return (
-    <div>
-      <h1>learning</h1>
-      <div className="flex justify-center items-center mt-2">
+    <div className="flex flex-col items-center justify-center p-6 bg-gray-200 min-h-screen">
+      <h1 className="text-3xl font-bold mb-6">Learning</h1>
+
+      <div className="flex justify-center items-center">
         <Link to="/">
-          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+          <button className="px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors duration-300">
             Go Home
           </button>
         </Link>
